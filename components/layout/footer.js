d3e78edddc3d924c19077c3b13e0d39f98aa8b1b
Lewis Jelfs <97512035+ljelfswebdev@users.noreply.github.com>
2022-04-15 09:41:38 +0200

    page set up

---
+++ components/layout/footer.js
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return ( 
+        <>
+        <footer className="bg-green text-center lg:text-left">
+            <div className="text-white text-center p-4">
+                Lewis Jelfs- Web Developer   
+            </div>
+        </footer>
+        </>
+     );
+}
+ 
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'green': '#097969'
+      }
+    },
   },
   plugins: [],
 }
